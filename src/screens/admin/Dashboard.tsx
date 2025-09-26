5a7c4a90106414c4cdb590107c19cc07d775c05b
João Suzuki <joaosuzuki98@outlook.com>
2025-09-26 20:37:08 -0300

    chore[sc-2]: Add layout to dashboard

--- src/screens/admin/Dashboard.tsx
+++ src/screens/admin/Dashboard.tsx
@@ -1,15 +1,8 @@
-import { Button } from '@react-navigation/elements'
-import { useNavigation } from '@react-navigation/native'
 import Layout from './Layout'
 
 function Dashboard() {
-    const navigation = useNavigation()
     return (
-        <Layout title='Dashboard'>
-            <Button onPress={() => navigation.navigate('AdminProfile')}>
-                Go to profile
-            </Button>
-        </Layout>
+        <Layout title='Dashboard' />
     )
 }
 
